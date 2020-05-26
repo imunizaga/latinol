2975d031303c9e137af53d7c0726420661a342a0
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2020-05-25 23:25:49 -0400

    Fix: Transcribe page correctly handles elements with text and children

--- src/latinol.js
+++ src/latinol.js
@@ -6,110 +6,117 @@
 /*global navigator: false */
 
 var Latinol = function() {
-    'use strict';
+  'use strict';
 
-    this.isCapital = function(letter) {
-        return letter === letter.toUpperCase();
+  this.isCapital = function(letter) {
+    return letter === letter.toUpperCase();
+  };
+
+  this.capitalizeAt = function(str, i) {
+    return str.substr(0, i) + str[i].toUpperCase() + str.substr(i + 1);
+  };
+
+  this.rules = {
+    'xc?': 'ks',
+    'cc([eiéí])': 'ks$1',
+    'c([aouráóú])': 'k$1',
+    'cl([aouráóú])': 'kl$1',
+    'c([eiéí])': 's$1',
+    'g([eiéí])': 'j$1',
+    'gu([eiéí])': 'g$1',
+    '\\b([^cs]|^)?h(\\S|$)': '$1$2',
+    'll': 'y',
+    'qu([eiéí])': 'k$1',
+    'ü': 'u',
+    'q([aouáóú])': 'k$1',
+    'w': 'gu',
+    'y([^aeiouáóúéí]|$)': 'i$1'
+  };
+
+  this.transcribe = function(text) {
+    var searchValue, replace, self, regExp, replaceRule;
+    self = this;
+
+    replace = function(oldValue) {
+      var newValue = oldValue.replace(regExp, replaceRule);
+
+      if (self.isCapital(oldValue[0])) {
+        if (newValue[0]) {
+          newValue = self.capitalizeAt(newValue, 0);
+        }
+      }
+
+      if (self.isCapital(oldValue[oldValue.length - 1])) {
+        if (newValue[1]) {
+          newValue = self.capitalizeAt(newValue, 1);
+        }
+      }
+      return newValue;
     };
 
-    this.capitalizeAt = function(str, i) {
-        return str.substr(0, i) + str[i].toUpperCase() + str.substr(i + 1);
-    };
+    for (searchValue in this.rules) {
+      if (this.rules.hasOwnProperty(searchValue)) {
+        regExp = new RegExp(searchValue, 'gi');
+        replaceRule = this.rules[searchValue];
+        text = text.replace(regExp, replace);
+      }
+    }
 
-    this.rules = {
-        'xc?': 'ks',
-        'cc([eiéí])': 'ks$1',
-        'c([aouráóú])': 'k$1',
-        'cl([aouráóú])': 'kl$1',
-        'c([eiéí])': 's$1',
-        'g([eiéí])': 'j$1',
-        'gu([eiéí])': 'g$1',
-        '\\b([^cs]|^)?h(\\S|$)': '$1$2',
-        'll': 'y',
-        'qu([eiéí])': 'k$1',
-        'ü': 'u',
-        'q([aouáóú])': 'k$1',
-        'w': 'gu',
-        'y([^aeiouáóúéí]|$)': 'i$1'
-    };
+    return text;
+  };
 
-    this.transcribe = function(text) {
-        var searchValue, replace, self, regExp, replaceRule;
-        self = this;
+  this.ignoreTags = {
+    'IFRAME': true,
+    'IMAGE': true,
+    'INPUT': true,
+    'LINK': true,
+    'META': true,
+    'SCRIPT': true,
+    'STYLE': true,
+    'NOSCRIPT': true
+  };
 
-        replace = function(oldValue) {
-            var newValue = oldValue.replace(regExp, replaceRule);
+  this.walk = function(node) {
+    // Source: http://is.gd/mwZp7E
 
-            if (self.isCapital(oldValue[0])) {
-                if (newValue[0]) {
-                    newValue = self.capitalizeAt(newValue, 0);
-                }
-            }
+    var child;
+    var next;
 
-            if (self.isCapital(oldValue[oldValue.length - 1])) {
-                if (newValue[1]) {
-                    newValue = self.capitalizeAt(newValue, 1);
-                }
-            }
-            return newValue;
-        };
+    if (this.ignoreTags[node.tagName]) {
+      return;
+    }
 
-        for (searchValue in this.rules) {
-            if (this.rules.hasOwnProperty(searchValue)) {
-                regExp = new RegExp(searchValue, 'gi');
-                replaceRule = this.rules[searchValue];
-                text = text.replace(regExp, replace);
-            }
+    switch (node.nodeType) {
+      case 1:  // Element
+        if (node.titl) {
+          node.title = this.transcribe(node.title);
         }
 
-        return text;
-    };
+        if (node.placeholder) {
+          node.placeholder = this.transcribe(node.placeholder);
+        }
 
-    this.ignoreTags = {
-      'IFRAME': true,
-      'IMAGE': true,
-      'INPUT': true,
-      'LINK': true,
-      'META': true,
-      'SCRIPT': true,
-      'STYLE': true,
-      'NOSCRIPT': true
-    };
+      case 9:  // Document
+      case 11: // Document fragment
+        child = node.firstChild;
+        while (child) {
+          next = child.nextSibling;
+          this.walk(child);
+          child = next;
+        }
 
-    this.transcribePage = function() {
-      var i;
+        break;
 
-      for (i = 0; i < document.children.length; i += 1) {
-        this.transcribeElement(document.children[i]);
-      }
-    };
+      case 3: // Text node
+        node.nodeValue = this.transcribe(node.nodeValue);
+        break;
+    }
+  };
 
-    this.transcribeElement = function(el) {
-      var i;
-
-      if (el.title != '') {
-        el.title = this.transcribe(el.title);
-      }
-
-      if (el.placeholder && el.placeholder != '') {
-        el.placeholder = this.transcribe(el.placeholder);
-      }
-
-      if (!el.innerHTML) {
-        return;
-      }
-
-      if (this.ignoreTags[el.tagName]) {
-        return;
-      }
-
-      if (!el.children.length && el.innerHTML) {
-        el.innerHTML = this.transcribe(el.innerHTML);
-        return;
-      }
-
-      for (i = 0; i < el.children.length; i += 1) {
-        this.transcribeElement(el.children[i]);
-      }
-    };
+  this.transcribePage = function() {
+    var _this = this;
+    setTimeout(function() {
+      _this.walk(document.body);
+    }, 1000);
+  };
 };
